dc06efb4a5d243831d4b9ea8808a9d8c2900bbb1
dev-loconav <99465109+dev-loconav@users.noreply.github.com>
2022-02-17 10:54:53 +0530

    Update Jenkinsfile_delete_rke.groovy

--- Jenkinsfile_delete_rke.groovy
+++ Jenkinsfile_delete_rke.groovy
@@ -7,7 +7,7 @@ pipeline {
                 echo "Deleting K8S cluster..."
                 ansiColor('xterm') {
                     ansiblePlaybook( 
-                        playbook: 'src/ansible/playbooks/delete_rke.yaml',
+                        playbook: 'src/ansible/delete_rke.yaml',
                         inventory: 'environments/prod/inventory/host.ini', 
                         colorized: true) 
                 }
