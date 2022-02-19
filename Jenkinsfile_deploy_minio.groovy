4bad79c9721858717bc7fa65b20d090b82b4c2c6
root <root@vm1.c3lqettl21ju5hbmrkwvl2bhhc.bx.internal.cloudapp.net>
2022-02-19 20:42:08 +0000

    Adiing Jenkinsfile_deploy_minio.groovy

---
+++ Jenkinsfile_deploy_minio.groovy
@@ -0,0 +1,18 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Deploy Minio') {
+            steps {
+                echo "Deploying Minio..."
+                ansiColor('xterm') {
+                    ansiblePlaybook( 
+                        playbook: 'src/ansible/deploy_minio.yaml',
+                        inventory: 'environments/prod/inventory/host.ini', 
+                        colorized: true) 
+                }
+            }
+        
+        }
+    }
+}
