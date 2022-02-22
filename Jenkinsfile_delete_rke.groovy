de8f082c38a9c5491debd21b53a9b7e47308fdbf
devopsexp <99752414+devopsexp@users.noreply.github.com>
2022-02-22 23:36:03 +0530

    Update Jenkinsfile_delete_rke.groovy

--- Jenkinsfile_delete_rke.groovy
+++ Jenkinsfile_delete_rke.groovy
@@ -5,6 +5,7 @@ pipeline {
         stage('Delete K8S Cluster') {
             steps {
                 echo "Deleting K8S cluster..."
+                sh "rm -rf /var/lib/jenkins/.ssh/known_hosts"
                 ansiColor('xterm') {
                     ansiblePlaybook( 
                         playbook: 'src/ansible/delete_rke.yaml',
