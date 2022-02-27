20a4ec945914d4bf60e390c8bb852ed0514b39ee
dev-loconav <99465109+dev-loconav@users.noreply.github.com>
2022-02-27 17:34:09 +0530

    Create Jenkinsfile_create_extend_fs.groovy

---
+++ Jenkinsfile_create_extend_fs.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+parameters {
+  string(name: 'hosts', defaultValue: 'all', description: 'Hosts on which FS to be created')
+
+
+ }
+    stages {
+        stage('Deploy Postgres') {
+            steps {
+                echo "Deploying Postgres..."
+                sh "rm -rf /var/lib/jenkins/.ssh/known_hosts"
+                ansiColor('xterm') {
+                    ansiblePlaybook( 
+                        playbook: 'src/ansible/create_lvm_fs.yaml',
+                        inventory: 'environments/prod/inventory/host.ini',
+                        limit:  "${params.hosts}",
+                        tags: "fs",
+                        colorized: true) 
+                }
+            }
+        
+        }
+    }
+    
+    post {
+      // Clean after build
+      always {
+          cleanWs()
+      }
+   }
+}
