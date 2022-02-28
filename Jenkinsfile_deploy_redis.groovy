cdf23a523a8ebf53106f71cfa67293c8956156b9
root <root@vm1.c3lqettl21ju5hbmrkwvl2bhhc.bx.internal.cloudapp.net>
2022-02-28 19:01:42 +0000

    Adding redis role

---
+++ Jenkinsfile_deploy_redis.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Deploy Redis') {
+            steps {
+                echo "Deploying Redis..."
+                sh "rm -rf /var/lib/jenkins/.ssh/known_hosts"
+                ansiColor('xterm') {
+                    ansiblePlaybook( 
+                        playbook: 'src/ansible/deploy_redis.yaml',
+                        inventory: 'environments/prod/inventory/host.ini', 
+                        colorized: true) 
+                }
+            }
+        
+        }
+    }
+    
+    post {
+      // Clean after build
+      always {
+          cleanWs()
+      }
+   }
+}
