0f45b1ffc0b407f17369dd7a0cf5375f5e116335
dev-loconav <99465109+dev-loconav@users.noreply.github.com>
2022-03-02 15:33:08 +0530

    Update format_minio_disks.sh

--- src/ansible/files/format_minio_disks.sh
+++ src/ansible/files/format_minio_disks.sh
@@ -5,7 +5,7 @@ for duids in `cat /etc/miniodiskuid`
 do
         dhost=`hostname`
         did=`blkid | grep $duids | cut -d":" -f1`
-        echo "["$dhost,$did"]" >> /tmp/diskhostmapping
+        echo $dhost,$did >> /tmp/diskhostmapping
    
 done
 cat /tmp/diskhostmapping
