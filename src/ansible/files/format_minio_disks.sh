3218e29d9b6fe908bccf008ea3ea5b1271e670d2
dev-loconav <99465109+dev-loconav@users.noreply.github.com>
2022-03-02 10:51:57 +0530

    Update format_minio_disks.sh

--- src/ansible/files/format_minio_disks.sh
+++ src/ansible/files/format_minio_disks.sh
@@ -4,6 +4,5 @@ for duids in `cat /etc/miniodiskuid`
 do
         dhost=`hostname`
         did=`blkid | grep $duids | cut -d":" -f1`
-        dcount=`kubectl directpv drives ls | grep -w $did | grep -w $dhost | grep "Available" | wc -l`
+        echo $dhost:$did >> /tmp/diskhostmapping
 done
-echo `cat /etc/miniodiskuid`
