9bf45b14e952ee6d2fe00d5f1c1bff713c7a3a31
dev-loconav <99465109+dev-loconav@users.noreply.github.com>
2022-03-02 16:53:37 +0530

    Delete f.sh

--- src/ansible/files/f.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dh=$1
-dd=$2
-dcount=`kubectl directpv drives ls | grep -w $dd | grep -w $dh | grep "Available" | wc -l`
-if [ $dcount == 1 ]
-then
-                kubectl directpv drives format --drives $dd --nodes $dh --force
-fi
