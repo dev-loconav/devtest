dcd1e2741184bc441212cc8d017e03ed7dab82b2
dev-loconav <99465109+dev-loconav@users.noreply.github.com>
2022-03-02 10:37:55 +0530

    Update format_minio_disks.sh

--- src/ansible/files/format_minio_disks.sh
+++ src/ansible/files/format_minio_disks.sh
@@ -1,14 +1,11 @@
-#!/bin/bash -xv
+#!/bin/bash
 
 for duids in `cat /etc/miniodiskuid`
 do
         dhost=`hostname`
         did=`blkid | grep $duids | cut -d":" -f1`
         dcount=`kubectl directpv drives ls | grep -w $did | grep -w $dhost | grep "Available" | wc -l`
-        echo $d
+        echo $dhost
+        echo $did
         echo $dcount
-        if [ $dcount == 1 ]
-        then
-                kubectl directpv drives format --drives $did --nodes $dhost --force
-        fi
 done
